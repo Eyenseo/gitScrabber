9361a6e5eff4b67935aa01d73c59905e290f1853
Andreas Poppele <andreas.poppele@freenet.de>
2017-06-21 11:00:29 +0200

    Added some comments

--- gitScrabber/scrabTasks/archive/languageDetector.py
+++ gitScrabber/scrabTasks/archive/languageDetector.py
@@ -5,25 +5,23 @@ version = "1.0.0"
 
 
 def languageDetector(report, project, global_args):
+    """
+    Tries to detect the programming language of a library 
+    based on the file extension
+
+    :param  report:       The report
+    :param  project:      The project
+    :param  global_args:  This task scrubber makes use of the github-token to
+                          circumvent the tight rate-limiting for the github
+                          api
+
+                          https://github.com/settings/tokens
+                          https://developer.github.com/v3/#authentication
+    """
     dir_ = project['location']
-    print(project)
 
-    number_of_files = 0
-    files_per_language = {
-        'C++':  0,
-        'C':    0,
-        'Rust': 0,
-        'Ruby': 0,
-        'Java': 0,
-        'Go':   0,
-        'PHP':  0,
-        'JavaScript':   0,
-        'Objective-C':  0,
-        'Swift':    0,
-        'C#':   0,
-        'Python':   0,
-    }
-
+    # dictionary containing the common file extensions 
+    # for each of the languages
     language_extensions = {
         'C++':  ['.cpp', '.c++', '.cc', '.C', '.cxx'],
         'C':    ['.c'],
@@ -39,23 +37,42 @@ def languageDetector(report, project, global_args):
         'Python':   ['.py', '.pyc', '.pyd', '.pyo', '.pyw', '.pyz']
     }
 
+    # count the files that have an extension of one of the languages
+    files_per_language = {
+        'C++':  0,
+        'C':    0,
+        'Rust': 0,
+        'Ruby': 0,
+        'Java': 0,
+        'Go':   0,
+        'PHP':  0,
+        'JavaScript':   0,
+        'Objective-C':  0,
+        'Swift':    0,
+        'C#':   0,
+        'Python':   0,
+    }
+
+    # walk through all files in the project and count extensions
     for dirpath, dirnames, filenames in os.walk(dir_):
         for file in filenames:
             filename, file_extension = os.path.splitext(file)
             for language in language_extensions:
                 if file_extension in language_extensions[language]:
                     files_per_language[language] += 1
-    print(files_per_language)
+    
+    # find the language with the maximal amount of files
     max_files = 0
     max_lang = 'Not detected'
     for language in files_per_language:
         if max_files < files_per_language[language]:
            max_lang = language
            max_files = files_per_language[language]
-    print(max_files)
-    print(max_lang)
+   
+    # as C and C++ share the same extensions .c we assume to have a C++
+    # project if there are at least 5 files with a C++ extension
     if 'C' is max_lang and files_per_language['C++'] > 5:
         max_lang = 'C++'
-    report['language'] = max_lang
 
-    # return {}
+    # write the result to the report
+    report['language'] = max_lang
